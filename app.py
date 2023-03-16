b10f9b91ef593509d876777fec1fe1b86ac9631e
Gorka Esteban Areizaga <gesteban@vicomtech.es>
2023-03-16 17:49:03 +0100

    solve problem

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
 from flask import Flask 
+from routes.routes_battery import battery_route
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+ 
+# Initialize Flask app
+app= Flask(__name__)
+
+db_url= 'localhost:5432'
+db_name= 'testApp_db'
+db_user= 'testApp'
+db_password= 'testApp'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{db_user}:{db_password}@{db_url}/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize Flask extensions
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+CORS(app)
 
 
-# Create the db and marshmallow objects
-db = SQLAlchemy()
-ma = Marshmallow()
-CORS()
-
-def create_app():
-    # Initialize Flask app
-    app= Flask(__name__)
-
-    db_url= 'localhost:5432'
-    db_name= 'testApp_db'
-    db_user= 'testApp'
-    db_password= 'testApp'
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{db_user}:{db_password}@{db_url}/{db_name}'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # Initialize the db and marshmallow objects
-    db.init_app(app)
-    ma.init_app(app)
-
-    # Register the blueprint
-    from backend.routes.routes_battery import battery_route
-    app.register_blueprint(battery_route)
-
-    return app
+# Register the blueprint
+app.register_blueprint(battery_route)
 
 if __name__ == '__main__':
-    app = create_app()
+    db.create_all()
     app.run(debug=True)
     
     
